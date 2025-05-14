4cfd93c9674856199d8ade9f66f021533dd5473c
tayyykaa12 <taisiyakapara@gmail.com>
2025-05-14 11:47:58 +0300

    Додав HTML та CSS файли

--- test_gui.py
+++ test_gui.py
@@ -21,7 +21,4 @@ ent.place(relx=0.1, rely=0.2)
 #  Кнопка
 but = Button(win, text='Click', font=('Comic Sans MS', 20), width=7, height=3, bg="red", fg='#00ff00', command=get_info)
 but.place(relx=0.1, rely=0.35)
-# Випадаючий список
-comb = ttk.Combobox(win, font=('Comic Sans MS', 15), width=10, values=['option1', 'option2', 'option3', 'option4'])
-comb.place(relx=0.4 ,rely=0.35)
-win.mainloop()
+
